924f1e819c85018a6f5583a023629375e4a2c357
joshuabenuck <0.0.0.0>
2010-07-03 14:52:45 -0700

    - Removed commented out code.

--- org.eshell.rubymonkey/scripts/eshell.rb
+++ org.eshell.rubymonkey/scripts/eshell.rb
@@ -8,26 +8,8 @@ addBundles(
    "org.eclipse.ui",
   ])
 
-#puts window.methods()
-#puts $LOAD_PATH
-#alert("testing")
-
-#puts(bundles["org.eclipse.jface"])
-#puts(bundles["org.eclipse.jdt.core"])
-#puts(bundles["org.eclipse.core.resources"])
-
 java_import org.eclipse.ui.console.ConsolePlugin
 java_import org.eclipse.ui.PlatformUI
-
-#puts ResourcesPlugin.getWorkspace().getRoot().findMember("${workspace_loc:/eshell/build.xml}")
-#runner = AntRunner.new()
-#runner.buildFileLocation = buildFiles[0].rawLocation.toFile().absolutePath
-#runner.availableTargets.each do |t|
-#    puts(t)
-#end
-#runner.setExecutionTargets(array(["test"], String))
-#runner.addBuildLogger("org.eclipse.ant.internal.ui.antsupport.logger.AntProcessBuildLogger")
-#runner.run()
 
 # Must not be run in UI thread!
 class BuildFileShell
@@ -161,7 +143,6 @@ class Eshell
       (ignored, tmp) = runStmt(stmt, shells)
       result = tmp == nil ? nil : tmp[0]
     end
-    #alert(stmt + "\r" + shell.class.name + "\r" + result.class.name)
     if result.class.name =~ /Shell$/
       shells.push(result)
       resultWasShell = true
@@ -191,15 +172,6 @@ class Eshell
     return nil
   end
 end
-
-# cp eshell ant eshell
-# cp eshell; ant; eshell; dist; test
-# ant eshell dist test
-# ant ls -- should this search parent shells? No?
-# SubShells cannot be the current shell and can only be used absolutely?
-# when to search parent shells?
-
-#Eshell.new().runCmd("cp eshell; server start")
 
 _run {
 begin
